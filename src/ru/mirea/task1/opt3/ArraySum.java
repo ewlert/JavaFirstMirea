c0bd3352def5008eb459db3d0828195090b337a5
ewlert <davidson321123@gmail.com>
2023-09-03 20:51:57 +0300

    Commit message

--- src/ru/mirea/task1/opt3/ArraySum.java
+++ src/ru/mirea/task1/opt3/ArraySum.java
@@ -1,5 +1,17 @@
 package ru.mirea.task1.opt3;
 
-public class ArraySum {
-
+class Main {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        int sum = 0;
+        arr[0] = 3;
+        arr[1] = 5;
+        arr[2] = 1;
+        arr[3] = 2;
+        arr[4] = 4;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        System.out.printf("Сумма массива %d \nСреднее арифметическое %d ", sum, sum / arr.length);
+    }
 }
