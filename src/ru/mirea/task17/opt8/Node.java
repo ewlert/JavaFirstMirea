2d520a54d892b33b5efb9b44a4d2b24e91d4111e
ewlert <davidson321123@gmail.com>
2023-11-19 15:13:11 +0300

    list 2

---
+++ src/ru/mirea/task17/opt8/Node.java
@@ -0,0 +1,53 @@
+package ru.mirea.task17.opt8;
+
+public class Node {
+    String data;
+    Node prev;
+    Node next;
+
+    public Node(String data) {
+        this.data = data;
+        this.prev = null;
+        this.next = null;
+    }
+}
+
+class DoublyLinkedList {
+    Node head;
+    Node tail;
+
+    public DoublyLinkedList() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    public void insertAtEnd(String data) {
+        Node newNode = new Node(data);
+        if (head == null) { // Если список пустой            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+    }
+
+    public void displayList() {
+        Node current = head;
+        System.out.print("List: ");
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        DoublyLinkedList list = new DoublyLinkedList();
+        list.insertAtEnd("Hello");
+        list.insertAtEnd("World");
+        list.displayList();
+    }
+}
